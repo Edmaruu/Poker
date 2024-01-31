c3bb038456a372a020544d61b0f6e22c078368d8
iedwin <inchaudanael@outlook.fr>
2024-01-31 10:10:55 +0100

    J'ai écris un truc au hasard pour modifier

---
+++ src/Cartes.java
@@ -0,0 +1,13 @@
+public enum Cartes {
+    AS(1),DEUX(2),TROIS(3),QUATRE(4),CINQ(5),SIX(6),SEPT(7),HUIT(8),NEUF(9),DIX(10),VALET(11),DAME(12),ROI(13);
+    private final int valeur;
+
+    Cartes(int valeur) {
+        this.valeur = valeur;
+    }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+}
---
+++ src/Paquets.java
@@ -0,0 +1,13 @@
+public class Paquets {
+
+    public String[] coeurs;
+    public Paquets(){
+
+        int forme = 1;
+        if (forme == 1) {
+            for (int i = 1; i < 14; i++) {
+             coeurs[i] = "AS";
+            }
+        }
+    }
+}
